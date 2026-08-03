7a1a4d04f7794f9638c737b50575c2300ce8c1bc
long <ab1234_1123@qq.com>
2026-08-03 20:01:02 +0800

    feat:add client sdk

--- client.go
+++ client.go
@@ -5,14 +5,43 @@ import (
 )
 
 type Client struct {
-	c *mrpc.Client
+	mc *mrpc.Client
 }
 
 func NewClient(address string, port int) *Client {
-	return &Client{
-		c: mrpc.NewClient(address, port),
-	}
+	return &Client{mc: mrpc.NewClient(address, port)}
 }
-func (cli*Client)Read(filename string,offset int,length int){
-	 
+func (cli *Client) Read(filename string, offset int, length int) {
+
+}
+func (cli *Client) Write(filename string, offset int, buf []byte) {
+
+}
+func (cli *Client) GetInfo(filename string) (*GetFileInfoReply, error) {
+	req := &GetFileInfoRequest{
+		Path: filename,
+	}
+	rep := &GetFileInfoReply{}
+	err := cli.mc.Call(MasterSerivceName+"."+GetFileInfo, req, rep)
+	if err != nil {
+		return nil, err
+	}
+	return rep, nil
+}
+func (cli *Client) Create(filename string) error {
+	req := &CreateFileRequest{
+		Path: filename,
+	}
+	rep := &CreateFileReply{}
+	return cli.mc.Call(MasterSerivceName+"."+CreateFile, req, rep)
+}
+func (cli *Client) Delete(filename string) error {
+	req := &DeleteFileRequest{
+		Path: filename,
+	}
+	rep := &DeleteFileReply{}
+	return cli.mc.Call(MasterSerivceName+"."+DeleteFile, req, rep)
+}
+func (cli *Client) Close() {
+	cli.mc.Close()
 }
